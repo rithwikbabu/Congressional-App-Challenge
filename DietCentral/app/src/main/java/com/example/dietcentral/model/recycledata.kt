101492ecab0d1b45efecb7c91e00a867223a0986
Rithwik Babu <54031798+rithwikbabu13@users.noreply.github.com>
2021-03-15 18:06:53 -0500

    Updated UI
    Added Recycleview and Temporary Logo
    Changed Theme colors

---
+++ DietCentral/app/src/main/java/com/example/dietcentral/model/recycledata.kt
@@ -0,0 +1,9 @@
+package com.example.dietcentral.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class recycledata(@StringRes val stringResourceId: Int,
+                       @DrawableRes val imageResourceId: Int){
+
+}
