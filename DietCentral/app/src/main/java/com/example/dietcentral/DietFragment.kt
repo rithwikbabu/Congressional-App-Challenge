53f4423116be5e86b9924aa8966d51479b3444c6
Rithwik Babu <54031798+rithwikbabu13@users.noreply.github.com>
2021-03-19 12:45:33 -0500

    Added Filter Fragment and Chips
    No Functionality

--- DietCentral/app/src/main/java/com/example/dietcentral/DietFragment.kt
+++ DietCentral/app/src/main/java/com/example/dietcentral/DietFragment.kt
@@ -4,10 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 
 class DietFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_diet, container, false)
+        var view =  inflater.inflate(R.layout.fragment_diet, container, false)
+
+
+        val b1 = view?.findViewById<Button>(R.id.button)
+
+        val FragTan: FragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
+
+        b1?.setOnClickListener {
+            FragTan.replace(R.id.fragment_container, fragment_filter());
+            FragTan.commit()
+        }
+        return view
     }
+
+
 }
---
+++ DietCentral/app/src/main/java/com/example/dietcentral/fragment_filter.kt
@@ -0,0 +1,21 @@
+package com.example.dietcentral
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dietcentral.adapter.RecyclerAdapter
+import kotlinx.android.synthetic.main.fragment_diet_7.*
+
+class fragment_filter : Fragment(){
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_filter, container, false)
+    }
+}
