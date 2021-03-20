ecbf29a0a923290a39fcfb7d25469e80fdb1fae6
Rithwik Babu <54031798+rithwikbabu13@users.noreply.github.com>
2021-03-20 15:46:45 -0500

    Added Adaptive Visualizer for Filter Chips

--- DietCentral/app/src/main/java/com/example/dietcentral/DietFragment.kt
+++ DietCentral/app/src/main/java/com/example/dietcentral/DietFragment.kt
@@ -7,11 +7,12 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 class DietFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view =  inflater.inflate(R.layout.fragment_diet, container, false)
-
 
         val b1 = view?.findViewById<Button>(R.id.button)
 
@@ -21,6 +22,27 @@ class DietFragment : Fragment() {
             FragTan.replace(R.id.fragment_container, fragment_filter());
             FragTan.commit()
         }
+
+        val chipgroup = view.findViewById<ChipGroup>(R.id.dietChipGroup)
+
+        fun updateChip(){
+            chipgroup.removeAllViews()
+            if (fragment_filter.selectedDietData.isNotEmpty()){
+                val chip = Chip(chipgroup.context)
+                chip.text = fragment_filter.selectedDietData[0]
+                chipgroup.addView(chip)
+            }
+            if (fragment_filter.selectedChipData.isNotEmpty()) {
+                for (x in fragment_filter.selectedChipData) {
+                    val chip = Chip(chipgroup.context)
+                    chip.text = x
+                    chipgroup.addView(chip)
+                }
+            }
+        }
+
+        updateChip()
+
         return view
     }
 
--- DietCentral/app/src/main/java/com/example/dietcentral/fragment_filter.kt
+++ DietCentral/app/src/main/java/com/example/dietcentral/fragment_filter.kt
@@ -1,71 +1,106 @@
 package com.example.dietcentral
 
+import android.R.attr.fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.CompoundButton
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
-import kotlinx.android.synthetic.main.fragment_diet.*
-import org.jetbrains.anko.find
-import org.w3c.dom.Text
 
 
 class fragment_filter : Fragment(){
-
-    var btnApply = null
-    var selectedChipData = mutableListOf<String>("")
+    companion object{
+        var selectedChipData = mutableListOf<String>()
+        var selectedDietData = mutableListOf<String>()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = inflater.inflate(R.layout.fragment_filter, container, false)
+        val view = inflater.inflate(R.layout.fragment_filter, container, false)
+        val view2 = inflater.inflate(R.layout.fragment_diet, container, false)
 
-        var chipfast = view.findViewById<Chip>(R.id.fastingchip)
-        var chipketo = view.findViewById<Chip>(R.id.ketochip)
-        var chipcarb = view.findViewById<Chip>(R.id.lowcarbchip)
-        var chipfat = view.findViewById<Chip>(R.id.lowfatchip)
-        var chippaleo = view.findViewById<Chip>(R.id.paleochip)
-        var chipmedit = view.findViewById<Chip>(R.id.mediterraneanchip)
-        var chipdash = view.findViewById<Chip>(R.id.dashchip)
-        var chipplant = view.findViewById<Chip>(R.id.plantbasedchip)
+        val b1 = view?.findViewById<Button>(R.id.backbutton)
 
-        var chipvegan = view.findViewById<Chip>(R.id.Vegan)
-        var chipvegetarian = view.findViewById<Chip>(R.id.Vegetarian)
-        var chipgluten = view.findViewById<Chip>(R.id.Gluten)
-        var chipwheat = view.findViewById<Chip>(R.id.Wheat)
-        var chipsoy = view.findViewById<Chip>(R.id.Soy)
-        var chipdairy = view.findViewById<Chip>(R.id.Dairy)
-        var chippeanut = view.findViewById<Chip>(R.id.Peanut)
-        var chipseafood = view.findViewById<Chip>(R.id.Seafood)
+        val FragTan: FragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
 
         var chipgroup1 = view.findViewById<ChipGroup>(R.id.chipGroup1)
         var chipgroup2 = view.findViewById<ChipGroup>(R.id.chipGroup2)
+        val chipgroup3 = view.findViewById<ChipGroup>(R.id.chipGroup3)
+        val chipgroup4 = view2.findViewById<ChipGroup>(R.id.dietChipGroup)
 
-        var textview1 = view.findViewById<TextView>(R.id.textView11)
+        fun updateChip(){
+            chipgroup3.removeAllViews()
+            if (selectedDietData.isNotEmpty()){
+                val chip = Chip(chipgroup3.context); val chip2 = Chip(chipgroup4.context)
+                chip.text = selectedDietData[0]; chip2.text = selectedDietData[0]
+                chipgroup3.addView(chip); chipgroup4.addView(chip2)
+            }
+            if (selectedChipData.isNotEmpty()) {
+                for (x in selectedChipData) {
+                    val chip = Chip(chipgroup3.context); val chip2 = Chip(chipgroup3.context)
+                    chip.text = x; chip2.text = x
+                    chipgroup3.addView(chip); chipgroup4.addView(chip2)
+                }
+            }
+        }
 
+        updateChip()
 
         val checkedChangeListener = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
                 selectedChipData.add(buttonView.getText().toString())
-                textview1.setText("It works")
+                updateChip()
             } else {
                 selectedChipData.remove(buttonView.getText().toString())
+                updateChip()
             }
         }
 
-        chipfast.setOnCheckedChangeListener(checkedChangeListener)
-        chipketo.setOnCheckedChangeListener(checkedChangeListener)
-        chipcarb.setOnCheckedChangeListener(checkedChangeListener)
-        chipfat.setOnCheckedChangeListener(checkedChangeListener)
-        chippaleo.setOnCheckedChangeListener(checkedChangeListener)
-        chipmedit.setOnCheckedChangeListener(checkedChangeListener)
-        chipdash.setOnCheckedChangeListener(checkedChangeListener)
-        chipplant.setOnCheckedChangeListener(checkedChangeListener)
+        val dietCheckedChangeListener = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                selectedDietData.add(buttonView.getText().toString())
+                updateChip()
+            } else {
+                if(selectedDietData.isNotEmpty()){
+                    selectedDietData.remove(buttonView.getText().toString())
+                    updateChip()
+                }
+            }
+        }
+
+        var chipfast = view.findViewById<Chip>(R.id.fastingchip); chipfast.setOnCheckedChangeListener(dietCheckedChangeListener)
+        var chipketo = view.findViewById<Chip>(R.id.ketochip); chipketo.setOnCheckedChangeListener(dietCheckedChangeListener)
+        var chipcarb = view.findViewById<Chip>(R.id.lowcarbchip); chipcarb.setOnCheckedChangeListener(dietCheckedChangeListener)
+        var chipfat = view.findViewById<Chip>(R.id.lowfatchip); chipfat.setOnCheckedChangeListener(dietCheckedChangeListener)
+        var chippaleo = view.findViewById<Chip>(R.id.paleochip); chippaleo.setOnCheckedChangeListener(dietCheckedChangeListener)
+        var chipmedit = view.findViewById<Chip>(R.id.mediterraneanchip); chipmedit.setOnCheckedChangeListener(dietCheckedChangeListener)
+        var chipdash = view.findViewById<Chip>(R.id.dashchip); chipdash.setOnCheckedChangeListener(dietCheckedChangeListener)
+        var chipplant = view.findViewById<Chip>(R.id.plantbasedchip); chipplant.setOnCheckedChangeListener(dietCheckedChangeListener)
+
+        var chipvegan = view.findViewById<Chip>(R.id.Vegan); chipvegan.setOnCheckedChangeListener(checkedChangeListener)
+        var chipvegetarian = view.findViewById<Chip>(R.id.Vegetarian); chipvegetarian.setOnCheckedChangeListener(checkedChangeListener)
+        var chipgluten = view.findViewById<Chip>(R.id.Gluten); chipgluten.setOnCheckedChangeListener(checkedChangeListener)
+        var chipwheat = view.findViewById<Chip>(R.id.Wheat); chipwheat.setOnCheckedChangeListener(checkedChangeListener)
+        var chipsoy = view.findViewById<Chip>(R.id.Soy); chipsoy.setOnCheckedChangeListener(checkedChangeListener)
+        var chipdairy = view.findViewById<Chip>(R.id.Dairy); chipdairy.setOnCheckedChangeListener(checkedChangeListener)
+        var chippeanut = view.findViewById<Chip>(R.id.Peanut); chippeanut.setOnCheckedChangeListener(checkedChangeListener)
+        var chipseafood = view.findViewById<Chip>(R.id.Seafood); chipseafood.setOnCheckedChangeListener(checkedChangeListener)
+
+        b1?.setOnClickListener {
+            FragTan.replace(R.id.fragment_container, DietFragment());
+            updateChip()
+            FragTan.commit()
+        }
 
         return view
+
     }
+
+
 
 
 }
